418c4c5dd91cf09a595a7c983d6af7f5416fa784
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2012-02-28 21:10:03 +0200

    refactored poster generation as methods, added quick test

--- lib/spillbot/poster.rb
+++ lib/spillbot/poster.rb
@@ -1,9 +1,15 @@
-meetup={"day"=>"28", "month"=>"02" ,"talk"=>{"title"=>"Workshop","speaker"=>"All","time_from"=>"19:00","time_to"=>"21:00"}}
-output = "poster.svg"
 require 'erb'
 
-template_file=File.join(File.dirname(__FILE__),'..','..','template','tmpl_thessrb.svg')
-template_content=File.read(template_file)
-template=ERB.new(template_content)
-svg=template.result
-File.open(output,"wb"){ |f| f.write(svg) }
+def generate_poster(meetup,template_file)
+  template_content=File.read(template_file)
+  template=ERB.new(template_content)
+  template.result(Proc.new{meetup}.binding)
+end
+
+if $0 == __FILE__
+  template_file=File.join(File.dirname(__FILE__),'..','..','template','tmpl_thessrb.svg')
+  meetup={"day"=>"28", "month"=>"02" ,"talk"=>{"title"=>"Workshop","speaker"=>"All","time_from"=>"19:00","time_to"=>"21:00"}}
+  output = "poster.svg"
+  svg=generate_poster(meetup,template_file)
+  File.open(output,"wb"){ |f| f.write(svg) }
+end
